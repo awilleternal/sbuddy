ca98ad53c4239ecdc0263a5742e5969d654de13f
awilleternal <ameywdg@gmail.com>
2024-11-26 23:03:23 +0530

    theme is installed made minor changes in most of functionality

--- base/views.py
+++ base/views.py
@@ -96,31 +96,39 @@ def userProfile(request,pk):
 
 @login_required(login_url='login')
 def createRoom(request):
-    
+    topics=Topic.objects.all()
     form=RoomForm()
     if request.method == 'POST':
-        form=RoomForm(request.POST)
-        if form.is_valid():
-            room=form.save(commit=False)
-            room.host=request.user
-            room.save()
-            return redirect('home')
+        topic_name=request.POST.get('topic')
+        topic,created=Topic.objects.get_or_create(name=topic_name)
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description')
+        )
+        
+        return redirect('home')
             
-    context={'form':form}
+    context={'form':form,'topics':topics}
     return render(request,'base/room_form.html',context)
 @login_required(login_url='login')
 def updateRoom(request,pk):
+    topics=Topic.objects.all()
     room=Room.objects.get(id=pk)
     form =RoomForm(instance=room)
     if request.user != room.host:
         return HttpResponse("you are not user")
         
     if request.method == 'POST':
-        form=RoomForm(request.POST,instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context={'form':form}
+        topic_name=request.POST.get('topic')
+        topic,created=Topic.objects.get_or_create(name=topic_name)
+        room.name=request.POST.get('name')
+        room.topic=topic
+        room.description=request.POST.get('description')
+        room.save()
+        return redirect('home')
+    context={'form':form,'topics':topics,'room':room}
     return render(request,'base/room_form.html',context)
 @login_required(login_url='login')       
 def deleteRoom(request,pk):
